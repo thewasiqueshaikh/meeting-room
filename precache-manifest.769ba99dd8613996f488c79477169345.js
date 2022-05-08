f887ab81b55263e42383f6da963e37afd82faf0a
thewasiqueshaikh <skwasique98@gmail.com>
2022-05-09 00:39:10 +0530

    Updates

---
+++ precache-manifest.769ba99dd8613996f488c79477169345.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7affc7c986709bb34a2",
+    "url": "/meeting-room/static/css/main.1d547820.chunk.css"
+  },
+  {
+    "revision": "d7affc7c986709bb34a2",
+    "url": "/meeting-room/static/js/main.d7affc7c.chunk.js"
+  },
+  {
+    "revision": "90ecf6b5710b2396cef7",
+    "url": "/meeting-room/static/js/1.90ecf6b5.chunk.js"
+  },
+  {
+    "revision": "8a092248929d2b12a00e",
+    "url": "/meeting-room/static/css/2.f06cdc8b.chunk.css"
+  },
+  {
+    "revision": "8a092248929d2b12a00e",
+    "url": "/meeting-room/static/js/2.8a092248.chunk.js"
+  },
+  {
+    "revision": "671c85d364d9bf27f8cd",
+    "url": "/meeting-room/static/js/runtime~main.671c85d3.js"
+  },
+  {
+    "revision": "ba02612db059533bc47ca41dbeabd3c4",
+    "url": "/meeting-room/index.html"
+  }
+];
